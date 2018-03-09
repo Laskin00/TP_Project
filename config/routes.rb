7d216c49e8d512ac9ca09311afcb2da1948ac647
Laskin00 <ale.laskintues19@gmail.com>
2018-03-09 08:39:47 +0200

    fixed style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get '', to: 'users#landing'
   get '/signup', to: 'users#new'
   resources :users
+  resources :warframes
+  resources :mods
+  resources :relics
+  resources :weapons
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
