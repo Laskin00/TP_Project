b6d864c110c0f3bb5d69f9abfd8222776f962de3
Laskin00 <ale.laskintues19@gmail.com>
2018-03-10 12:18:48 +0200

    User index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Example User", email: "ale.laskin@gmail.com", password: "123123", password_confirmation: "123123", image_url: "https://www.mobafire.com/images/champion/square/master-yi.png")
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+n+1}@railstutorial.org"
+  password = "password"
+  image_url = "https://www.mobafire.com/images/champion/square/master-yi.png"
+  User.create!(name:name, email:email, password: password, password_confirmation: password, image_url: image_url)
+end
