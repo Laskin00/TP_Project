e0493564249ff8f08f7c2ef64e59ea996d208385
Laskin00 <ale.laskintues19@gmail.com>
2018-03-12 09:46:11 +0200

    types to relics and mods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311105749) do
+ActiveRecord::Schema.define(version: 20180312074509) do
 
   create_table "mods", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180311105749) do
     t.string "dropChance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "mod_type"
   end
 
   create_table "relics", force: :cascade do |t|
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180311105749) do
     t.string "dropChance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "relic_type"
   end
 
   create_table "users", force: :cascade do |t|
