69449708397ec30a8625368fef586b8b337add11
Laskin00 <ale.laskintues19@gmail.com>
2018-05-24 22:39:42 +0300

    fixes

--- app/models/relic.rb
+++ app/models/relic.rb
@@ -1,6 +1,6 @@
 class Relic < ApplicationRecord
   has_many :relic_primepart_connections
   has_many :primeparts, through: :relic_primepart_connections
-  has_many :favorite_mods, dependent: :destroy
-  has_many :users, through: :favorite_mods
+  has_many :favorite_relics, dependent: :destroy
+  has_many :users, through: :favorite_relics
 end
